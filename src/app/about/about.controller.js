d71ec6161dfdfb012a45049cb7564fda9c0d34ef
krbz'11 <karby92@gmail.com>
2015-07-06 20:48:43 +0200

    RWD

--- src/app/about/about.controller.js
+++ src/app/about/about.controller.js
@@ -6,23 +6,41 @@
     .controller('AboutController', AboutController);
 
     function AboutController() {
-      var skill = document.querySelectorAll('.skill');
-
-      for (var i=0; i < skill.length; i++) {
-        var percent = skill[i].querySelector('i').getAttribute('data-percent');
-        skill[i].querySelector('div span').style.width = percent + '%';
+       
+      function skillBarsAnimation() {
+        for (var i=0; i < skill.length; i++) {
+          var percent = skill[i].querySelector('i').getAttribute('data-percent');
+          skill[i].querySelector('div span').style.width = percent + '%';
+        }
       }
+
+
+      function resetSkillBarAnimation() {
+        for (var i=0; i < skill.length; i++) {
+          skill[i].querySelector('div span').style.width = '0%';
+        }
+        setTimeout(skillBarsAnimation, 300);
+      }
+
+      var skill = document.querySelectorAll('.skill');
+      setTimeout(skillBarsAnimation, 500)
       
-
-
       var switcherValue = false;    
     	document.querySelector('.switcher').addEventListener('click', function(){
 
     		switcherValue = !switcherValue;
-	    	if (switcherValue) {
+	    	
+        if (switcherValue) {
+        
+          resetSkillBarAnimation();
+
 	    		document.querySelector('body').classList.add("black");
-	    	} else {
-	    		document.querySelector('body').classList.remove("black");          
+	    	
+        } else {
+
+          resetSkillBarAnimation();
+  	      
+      		document.querySelector('body').classList.remove("black");          
           document.querySelector('body').classList.add("white");
    		
 	    	}
